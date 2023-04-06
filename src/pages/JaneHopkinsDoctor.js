da72238968ead9e965af049e62f44e415a5d28b1
Anish J Mishra <56990766+anishJMishra@users.noreply.github.com>
2023-04-05 20:22:50 -0700

    Added Loading wheel for PatientTable

--- src/pages/JaneHopkinsDoctor.js
+++ src/pages/JaneHopkinsDoctor.js
@@ -4,7 +4,13 @@ import TopBanner from "../components/TopBanner";
 import AddPatient from "../components/AddPatient";
 import useJaneHopkins from "../hooks/useJaneHopkins";
 import PatientTable from "../components/PatientTable";
-import { Button, Stack, Typography, Box } from "@mui/material";
+import {
+  Button,
+  Stack,
+  Typography,
+  Box,
+  CircularProgress,
+} from "@mui/material";
 import SideBanner from "../components/SideBanner";
 import PatientDisplay from "../components/PatientDisplay";
 import { ApolloProvider } from "@apollo/client";
@@ -12,72 +18,84 @@ import client from "../components/apolloClient";
 import { useNavigate } from "react-router-dom";
 import PatientPopout from "../components/PatientPopout";
 
-
 const JaneHopkinsDoctor = () => {
-    const { entities } = useJaneHopkins();
+  const { entities } = useJaneHopkins();
 
-    console.log("Entities in App:", entities);
-    console.log("Patient entity:", entities.patient);
+  const [loading, setLoading] = useState(true);
 
-    const [selectedPatient, setSelectedPatient] = useState(null);
-    const [isPopoutOpen, setIsPopoutOpen] = useState(false);
-  
-    const [patients, setPatients] = useState([]);
+  console.log("Entities in App:", entities);
+  console.log("Patient entity:", entities.patient);
 
-    const handlePatientClick = (patient) => {
-      setSelectedPatient(patient);
-      setIsPopoutOpen(true);
+  const [selectedPatient, setSelectedPatient] = useState(null);
+  const [isPopoutOpen, setIsPopoutOpen] = useState(false);
+
+  const [patients, setPatients] = useState([]);
+
+  const handlePatientClick = (patient) => {
+    setSelectedPatient(patient);
+    setIsPopoutOpen(true);
+  };
+
+  const handlePopoutClose = () => {
+    setIsPopoutOpen(false);
+    setSelectedPatient(null);
+  };
+
+  useEffect(() => {
+    const fetchPatients = async () => {
+      try {
+        const response = await entities.patient.list();
+        console.log("Response:", response);
+        setPatients(response.items);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching patients:", error);
+      }
     };
 
-    const handlePopoutClose = () => {
-      setIsPopoutOpen(false);
-      setSelectedPatient(null);
-    };
+    fetchPatients();
+  }, [entities]);
 
-    useEffect(() => {
-      const fetchPatients = async () => {
-        try {
-          const response = await entities.patient.list();
-          console.log("Response:", response);
-          setPatients(response.items);
-        } catch (error) {
-          console.error("Error fetching patients:", error);
-        }
-      };
-  
-      fetchPatients();
-    }, [entities]);
-  
-   
   const navigate = useNavigate();
 
   const handleLogout = () => {
     navigate("/");
-  }
-   
+  };
 
   return (
     <div>
-      
-    <TopBanner />
-    <Navbar onLogout={handleLogout} />
-    <Stack direction="row">
-      <SideBanner />
-      <Box>
-     
-     
-      </Box>
-    </Stack>
-    <h1>Patient List</h1>
-    <Button>View Patients</Button>
-    <PatientTable patients={patients} onPatientClick={handlePatientClick} />
-      <PatientPopout
-        isOpen={isPopoutOpen}
-        handleClose={handlePopoutClose}
-        patient={selectedPatient}
-      />
-  </div>
-  )
-}
+      <TopBanner />
+      <Navbar onLogout={handleLogout} />
+      <Stack direction="row">
+        <SideBanner />
+        <Box></Box>
+      </Stack>
+      <h1>Patient List</h1>
+      <Button>View Patients</Button>
+      {loading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="200px"
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          <PatientTable
+            patients={patients}
+            onPatientClick={handlePatientClick}
+          />
+          <PatientPopout
+            isOpen={isPopoutOpen}
+            handleClose={handlePopoutClose}
+            patient={selectedPatient}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default JaneHopkinsDoctor;
